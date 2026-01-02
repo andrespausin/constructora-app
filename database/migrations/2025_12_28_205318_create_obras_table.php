f11aaee80d07d7e7eebd8aeb2a1aecff4f2d90cb
Cesar Pausin WSL <candrespausin2001@gmail.com>
2026-01-02 18:22:16 +0100

    edit: modified obras_table structure

--- database/migrations/2025_12_28_205318_create_obras_table.php
+++ database/migrations/2025_12_28_205318_create_obras_table.php
@@ -14,10 +14,17 @@ return new class extends Migration
         Schema::create('obras', function (Blueprint $table) {
             $table->string('id_obra', 20)->primary()->unique();
             $table->string('nombre_obra', 150);
-            $table->string('direccion', 255)->nullable();
+            $table->string('id_cliente');
+            $table->foreign('id_cliente')->references('id_cliente')->on('clientes');
+
             $table->string('provincia', 100)->nullable();
+            $table->string('direccion', 255)->nullable();
+            
             $table->date('fecha_inicio')->nullable();
             $table->date('fecha_fin')->nullable();
+
+            $table->enum('status', ['EN CURSO', 'NO PLANIFICADA', 'FINALIZADA'])->default('NO PLANIFICADA');
+            
             $table->timestamps();
         });
     }
