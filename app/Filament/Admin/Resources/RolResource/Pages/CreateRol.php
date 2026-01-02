865eb493e55aef36b16c7c24006a0544865159ea
Cesar Pausin WSL <candrespausin2001@gmail.com>
2026-01-02 17:23:21 +0100

    add: creteRol view

---
+++ app/Filament/Admin/Resources/RolResource/Pages/CreateRol.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Admin\Resources\RolResource\Pages;
+
+use App\Filament\Admin\Resources\RolResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateRol extends CreateRecord
+{
+    protected static string $resource = RolResource::class;
+}
